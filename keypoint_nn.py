24dfd05c7746e3604dffc835bd959d7cfd8cd05d
Yongjian Tang <118395620+markustyj@users.noreply.github.com>
2022-11-19 18:11:57 +0100

    i2dl ws21
    test2

---
+++ keypoint_nn.py
@@ -0,0 +1,92 @@
+"""Models for facial keypoint detection"""
+
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+
+class KeypointModel(pl.LightningModule):
+    """Facial keypoint detection model"""
+    def __init__(self, hparams):
+        """
+        Initialize your model from a given dict containing all your hparams
+        Warning: Don't change the method declaration (i.e. by adding more
+            arguments), otherwise it might not work on the submission server
+        """
+        super(KeypointModel, self).__init__()
+        self.hparams = hparams
+        ########################################################################
+        # TODO: Define all the layers of your CNN, the only requirements are:  #
+        # 1. The network takes in a batch of images of shape (Nx1x96x96)       #
+        # 2. It ends with a linear layer that represents the keypoints.        #
+        # Thus, the output layer needs to have shape (Nx30),                   #
+        # with 2 values representing each of the 15 keypoint (x, y) pairs      #
+        #                                                                      #
+        # Some layers you might consider including:                            #
+        # maxpooling layers, multiple conv layers, fully-connected layers,     #
+        # and other layers (such as dropout or batch normalization) to avoid   #
+        # overfitting.                                                         #
+        ########################################################################
+        
+        self.model = None
+        self.model = nn.Sequential(
+            nn.Conv2d(1,5,5),  #5x92x92
+            nn.Conv2d(5,10,5), #10x88x88
+            nn.MaxPool2d(2,2), #10x44x44
+            nn.Conv2d(10,10,5),  #10x40x40
+            nn.Conv2d(10,8,9),  #8x32x32
+            nn.MaxPool2d(2,2), #8x16x16
+            nn.Conv2d(8,16,7), #16x10x10
+            nn.MaxPool2d(2,2), #16x5x5
+            nn.Conv2d(16,6,3), #6x3x3
+            nn.Flatten(),
+            nn.Linear(54,30)
+            
+        )
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+
+    def forward(self, x):
+        
+        # check dimensions to use show_keypoint_predictions later
+        if x.dim() == 3:
+            x = torch.unsqueeze(x, 0)
+        ########################################################################
+        # TODO: Define the forward pass behavior of your model                 #
+        # for an input image x, forward(x) should return the                   #
+        # corresponding predicted keypoints                                    #
+        ########################################################################
+        x = self.model(x)
+        
+
+        ########################################################################
+        #                           END OF YOUR CODE                           #
+        ########################################################################
+        return x
+
+
+class DummyKeypointModel(pl.LightningModule):
+    """Dummy model always predicting the keypoints of the first train sample"""
+    def __init__(self):
+        super().__init__()
+        self.prediction = torch.tensor([[
+            0.4685, -0.2319,
+            -0.4253, -0.1953,
+            0.2908, -0.2214,
+            0.5992, -0.2214,
+            -0.2685, -0.2109,
+            -0.5873, -0.1900,
+            0.1967, -0.3827,
+            0.7656, -0.4295,
+            -0.2035, -0.3758,
+            -0.7389, -0.3573,
+            0.0086, 0.2333,
+            0.4163, 0.6620,
+            -0.3521, 0.6985,
+            0.0138, 0.6045,
+            0.0190, 0.9076,
+        ]])
+
+    def forward(self, x):
+        return self.prediction.repeat(x.size()[0], 1, 1, 1)
